87eec13bb375ea82e1f82786c5a44f16e62fbcea
Sean Fooks <seanfooks@gmail.com>
2015-07-13 14:06:27 -0700

    finished most of it but need some help

--- lab1.js
+++ lab1.js
@@ -91,7 +91,14 @@ sentence1 = array1.join(" ") + ".";
 // TODO: part #2: use a while or do-while loop to replace the words in sentence 2
 // with 'chirp' (10 points)
 
-// your code goes here
+var array2 = sentence2.split(" ");
+var i = 0;
+while (i < array2.length){
+  array2[i] = 'chirp'
+  i++;
+};
+
+sentence2 = array2.join(" ") + ".";
 
 // Leave these assertions as-is! If they pass, your code works.
 assert(sentence1 === 'chirp chirp chirp.', 'sentence 1 should have 3 chirps');
@@ -113,7 +120,8 @@ var nextAnimal;
 // TODO: 10 points
 // Assign one of your favorite animals to nextAnimal using Math.random() to pick
 
-// your code goes here
+var i = Math.floor(Math.random() * favoriteAnimals.length);
+nextAnimal = favoriteAnimals[i];
 
 assert(nextAnimal, 'assign something to nextAnimal');
 
@@ -130,11 +138,11 @@ assert(nextAnimal, 'assign something to nextAnimal');
 // don't require quotes for the code to work. Remove the unnecessary quotes.
 
 var animalExhibitStats = {
-  'numberOpen': 13,
+  numberOpen: 13,
   'number closed': 2,
-  'petting-zoo-open': true,
-  'mostPopular': 'Lucky the Emperor Penguin',
-  '2ndMostPopular': 'Dumbo the Depressed Donkey'
+  petting-zoo-open: true,
+  mostPopular: 'Lucky the Emperor Penguin',
+  2ndMostPopular: 'Dumbo the Depressed Donkey'
 };
 
 /*
@@ -144,17 +152,17 @@ var animalExhibitStats = {
  notation with dot notation wherever possible.
 */
 
-assert(animalExhibitStats['numberOpen'] === 13, 'there should be 13 open exhibits');
+assert(animalExhiibitStats.numberOpen === 13, 'there should be 13 open exhibits');
 assert(animalExhibitStats['number closed'] === 2, 'there should be 2 closed exhibits');
-assert(animalExhibitStats['petting-zoo-open'], 'hey! =( i was promised meerkats!');
-assert(animalExhibitStats['2ndMostPopular'] === 'Dumbo the Depressed Donkey',
+assert(animalExhibitStats.petting-zoo-open, 'hey! =( i was promised meerkats!');
+assert(animalExhibitStats.2ndMostPopular === 'Dumbo the Depressed Donkey',
   'Dumbo is supposed to be second place, as usual');
 
 // TODO: 4 points
 // Write an assert about the 'mostPopular' property of animalExibitStats.
 // Use the above examples to guide you.
 
-// your assert goes here
+assert(animalExibitStats.mostPopular, 'Lucky the Emperor Penguin', 'What gives!')
 
 /* ==================================================================
 ----------------- Code Style (10 points) ----------------------------
